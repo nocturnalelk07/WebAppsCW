255a0cafbb7933c3369f7d21d422f54cd7e53b42
nocturnalelk07 <finnp@outlook.com>
2024-10-19 15:03:17 +0100

    tested seeders with animal table

---
+++ database/seeders/AnimalTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Animal;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AnimalTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $a = new Animal;
+        $a->name = "Holly";
+        $a->save();
+    }
+}
