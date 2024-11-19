0c342f27a56472652e370013f8c589a69d59da41
nocturnalelk07 <finnp@outlook.com>
2024-11-19 12:19:15 +0000

    implemented /test route that works

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/users', function () {
+    return view('users');
+});
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
