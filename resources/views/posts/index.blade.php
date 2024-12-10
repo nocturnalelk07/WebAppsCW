9bcbad2df4bb63a7e8c38c67857ce9508a769a62
nocturnalelk07 <finnp@outlook.com>
2024-12-10 21:25:18 +0000

    posts can be reached by navigation bar

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,20 +1,3 @@
-<!--
-@extends("layouts.pageLayout")
-
-@section("title", "Posts")
-
-@section("content")
-    <div>
-        <a href="{{ route("posts.create")}}"> make your own post!</a>
-    </div>
-    <p>posts: </p>
-    <ul>
-        @foreach ($posts as $post)
-            <li><a href="{{ route("posts.show", ["id" => $post->id]) }}"> {{$post->post_title}}</a></li>
-        @endforeach
-    </ul>        
-@endsection   
--->
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
